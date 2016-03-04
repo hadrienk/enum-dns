e4baf28ad2c45456bfeda503a9ab09d5653e530e
Hadrien Kohl <hadrien.kohl@teletopia.com>
2016-03-04 19:28:21 +0100

    Glue the prefix handler to the backend

--- enum/rest/http.go
+++ enum/rest/http.go
@@ -26,11 +26,11 @@ func CreateHttpHandlerFor(b *enum.Backend) http.Handler {
 	prefixRe := "[1-9][0-9]{0,13}"
 	limitRe := "-?[1-9][0-9]*"
 
+	router.HandleFunc(fmt.Sprintf("/interval/{prefix:%s}", prefixRe), http.IntervalForPrefixHandler)
 	router.HandleFunc(fmt.Sprintf("/interval/{from:%s}", numRe), http.IntervalForNumberHandler)
+	router.HandleFunc(fmt.Sprintf("/interval/{prefix:%s},{limit:%s}", prefixRe, limitRe), http.IntervalForPrefixHandler)
 	router.HandleFunc(fmt.Sprintf("/interval/{from:%s},{to:%s}", numRe, numRe), http.IntervalForNumberHandler)
 	router.HandleFunc(fmt.Sprintf("/interval/{from:%s},{to:%s},{limit:%s}", numRe, numRe, limitRe), http.IntervalForNumberHandler)
-	router.HandleFunc(fmt.Sprintf("/interval/{prefix:%s}", prefixRe), http.IntervalForPrefixHandler)
-	router.HandleFunc(fmt.Sprint("/interval/{prefix:%s},{limit:%s}", prefixRe, limitRe), http.IntervalForPrefixHandler)
 
 	http.handler = router
 
@@ -42,10 +42,30 @@ func (h *HttpEndpoint) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *HttpEndpoint) IntervalForPrefixHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "IntervalForPrefixHandler:\n %+v", r)
+
+	vars := mux.Vars(r)
+
+	prefix, err := strconv.ParseUint(vars["prefix"], 10, 64)
+	limit, err := strconv.ParseInt(vars["limit"], 10, 32)
+
+	if limit == 0 {
+		limit = 100
+	}
+
+	from, err := enum.PrefixToE164(prefix)
+	to, err := enum.PrefixToE164(prefix + 1)
+
+	results, err := h.backend.RangesBetween(from, to, int(limit))
+
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+	} else {
+		json.NewEncoder(w).Encode(results)
+	}
 }
 
 func (h *HttpEndpoint) IntervalForNumberHandler(w http.ResponseWriter, r *http.Request) {
+
 	vars := mux.Vars(r)
 
 	from, err := strconv.ParseUint(vars["from"], 10, 64)
--- enum/utils.go
+++ enum/utils.go
@@ -1,6 +1,8 @@
 package enum
 
 import (
+	"errors"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -20,4 +22,18 @@ func ConvertEnumToInt(enum string) (uint64, error) {
 	enum = strings.Replace(enum, ".", "", -1)
 	enum = Reverse(enum)
 	return strconv.ParseUint(enum, 10, 64)
+}
+
+// Make any number 15 digits long by padding zeros
+func PrefixToE164(number uint64) (uint64, error) {
+	// Standardize the input.
+	if !(0 < number && number < 1000000000000000) {
+		return 0, errors.New("Number is outside the range [1:10^15]")
+	}
+
+	// 1234 -> 123400000000000 (E164).
+	number = uint64(float64(number) * math.Pow10(int(14-math.Floor(math.Log10(float64(number))))))
+
+	return number, nil
+
 }
