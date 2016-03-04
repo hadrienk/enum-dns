ef338aa08d1b23da9159f69983a024e4ef8c3e5a
Hadrien Kohl <hadrien.kohl@teletopia.com>
2016-03-04 18:57:07 +0100

    Add crude HTTP implementation

---
+++ enum/rest/http.go
@@ -0,0 +1,62 @@
+package rest
+
+import (
+	"encoding/json"
+	"enum-dns/enum"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+)
+
+type HttpEndpoint struct {
+	backend enum.Backend
+	handler http.Handler
+}
+
+func CreateHttpHandlerFor(b *enum.Backend) http.Handler {
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	http := HttpEndpoint{
+		backend: *b,
+	}
+
+	numRe := "[1-9][0-9]{0,14}"
+	prefixRe := "[1-9][0-9]{0,13}"
+	limitRe := "-?[1-9][0-9]*"
+
+	router.HandleFunc(fmt.Sprintf("/interval/{from:%s}", numRe), http.IntervalForNumberHandler)
+	router.HandleFunc(fmt.Sprintf("/interval/{from:%s},{to:%s}", numRe, numRe), http.IntervalForNumberHandler)
+	router.HandleFunc(fmt.Sprintf("/interval/{from:%s},{to:%s},{limit:%s}", numRe, numRe, limitRe), http.IntervalForNumberHandler)
+	router.HandleFunc(fmt.Sprintf("/interval/{prefix:%s}", prefixRe), http.IntervalForPrefixHandler)
+	router.HandleFunc(fmt.Sprint("/interval/{prefix:%s},{limit:%s}", prefixRe, limitRe), http.IntervalForPrefixHandler)
+
+	http.handler = router
+
+	return &http
+}
+
+func (h *HttpEndpoint) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.handler.ServeHTTP(w, r)
+}
+
+func (h *HttpEndpoint) IntervalForPrefixHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "IntervalForPrefixHandler:\n %+v", r)
+}
+
+func (h *HttpEndpoint) IntervalForNumberHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	from, err := strconv.ParseUint(vars["from"], 10, 64)
+	to, err := strconv.ParseUint(vars["to"], 10, 64)
+	limit, err := strconv.ParseInt(vars["limit"], 10, 32)
+
+	results, err := h.backend.RangesBetween(from, to, int(limit))
+
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+	} else {
+		json.NewEncoder(w).Encode(results)
+	}
+}
