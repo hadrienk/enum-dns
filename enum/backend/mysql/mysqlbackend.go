ca505ae16ce232eaf710098060bd81a73e328fd4
Hadrien Kohl <hadrien.kohl@teletopia.com>
2016-03-17 18:32:55 +0100

    Implement new interface for mysql

--- enum/backend/mysql/mysqlbackend.go
+++ enum/backend/mysql/mysqlbackend.go
@@ -21,20 +21,52 @@ func (b mysqlbackend) Close() error {
 	return b.database.Close()
 }
 
-func (b mysqlbackend) RemoveRange(r NumberRange) error {
-	return nil
-}
-func (b mysqlbackend) AddRange(r NumberRange) ([]NumberRange, error) {
+func (b mysqlbackend) PushRange(r NumberRange) ([]NumberRange, error) {
 	return nil, nil
 }
-func (b mysqlbackend) RangeFor(number uint64) (NumberRange, error) {
-	row := b.database.QueryRow("select lower, upper, regex from enum where lower <= ? AND ? <= upper", number, number)
-	result := new(NumberRange)
-	err := row.Scan(&result.Lower, &result.Upper, &result.Regexp)
+
+func (b mysqlbackend) RangesBetween(l, u uint64, c int) (NumberRange, error) {
+
+	queryTemplate := `SELECT i.lower,
+							i.upper,
+							r.order,
+							r.preference,
+							r.flags,
+							r.service,
+							r.regexp,
+							r.replacement
+						FROM interval i INNER JOIN records r
+						ON r.lower = i.lower AND r.upper = i.upper
+						WHERE ? <= i.lower  AND i.upper <= ? LIMIT ?;`
+
+	rows, err := b.database.Query(queryTemplate, l, u, c)
 	if err != nil {
-		fmt.Printf("[ERR] dns: database error: %v", err)
-		return *result, err
-	} else {
-		return *result, nil
+		return err
 	}
+	defer rows.Close()
+
+	results := make([]NumberRange, c)
+	lastRange := new(NumberRange)
+	for rows.Next() {
+		record := new(Record)
+		newRange := new(NumberRange)
+		err := rows.Scan(
+			newRange.Lower, newRange.Lower,
+			record.Order, record.Preference,
+			record.Flags, record.Service,
+			record.Regexp, record.Replacement,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		if !lastRange.Equals(newRange) {
+			lastRange = newRange
+			newRange.Records = make([]Record, 10)
+			append(results, newRange)
+		}
+		append(lastRange.Records, record)
+	}
+
+	return results, err
 }
